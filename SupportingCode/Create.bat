13e095550da6a2221f33224c1b3e957c7d83a7cc
RBryant57 <rwbryant57@outlook.com>
2020-10-12 18:25:40 -0700

    Added workspace.

--- SupportingCode/Create.bat
+++ SupportingCode/Create.bat
@@ -4,6 +4,8 @@ dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
 dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet tool install --global dotnet-aspnet-codegenerator
+
+dotnet ef dbcontext scaffold "Server=.\Development;Database=CommuteTracker;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models
 dotnet aspnet-codegenerator controller -name CommutesController -async -api -m Commute -dc CommuteTrackerContext -outDir Controllers
 dotnet aspnet-codegenerator controller -name CommuteLegsController -async -api -m CommuteLeg -dc CommuteTrackerContext -outDir Controllers
 dotnet aspnet-codegenerator controller -name DelayReasonsController -async -api -m DelayReason -dc CommuteTrackerContext -outDir Controllers
@@ -12,3 +14,5 @@ dotnet aspnet-codegenerator controller -name FareClassesController -async -api -
 dotnet aspnet-codegenerator controller -name PassConditionsController -async -api -m PassCondition -dc CommuteTrackerContext -outDir Controllers
 dotnet aspnet-codegenerator controller -name RoutesController -async -api -m Route -dc CommuteTrackerContext -outDir Controllers
 dotnet aspnet-codegenerator controller -name RouteTypesController -async -api -m RouteType -dc CommuteTrackerContext -outDir Controllers
+
+dotnet publish -o D:\Webs\YCDServices\CommuteTrackerService
