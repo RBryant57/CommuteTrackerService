7df95d22926f3eb17faac6f38049440295e51ecd
Richard Bryant <rwbryant57@outlook.com>
2021-02-02 19:06:45 -0800

    Added VS solution.

---
+++ CommuteTrackerServiceTests/AbstractControllerTest.cs
@@ -0,0 +1,21 @@
+using EntityLayer.Contexts;
+using EntityLayer.Interfaces;
+using EntityLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CommuteTrackerServiceTests
+{
+    public abstract class AbstractControllerTest
+    {
+        protected readonly CommuteTrackerContext context;
+        public AbstractControllerTest()
+        {
+            var options = new DbContextOptionsBuilder<CommuteTrackerContext>()
+                .UseInMemoryDatabase("CommuteTrackerContext", new InMemoryDatabaseRoot())
+                .Options;
+
+            //context = new CommuteTrackerContext(options);
+        }
+    }
+}
---
+++ CommuteTrackerServiceTests/RouteTypesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Models;
+using CommuteTrackerService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommuteTrackerServiceTests
+{
+    public class RouteTypesControllerTests : AbstractControllerTest
+    {
+        [Fact]
+        public void GetRouteTypes_Succeeds()
+        {
+            // Arrange
+            var controller = new RouteTypesController(context);
+
+            // Act
+            var response = controller.GetRouteType();
+            var value = response.Result.Value;
+
+            // Assert
+            //Assert.IsType<List<RouteType>>(value);
+        }
+    }
+}
